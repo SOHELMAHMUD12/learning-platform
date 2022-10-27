e49485153524ae2ad5f98f0feb1e9ba1468e2f87
SOHELMAHMUD12 <sohelcse123@gmail.com>
2022-10-27 12:15:53 +0600

    added blog

--- src/Components/UserContext/UserContext.js
+++ src/Components/UserContext/UserContext.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const Layout = () => {
+const UserContext = () => {
     return (
         <div>
             
@@ -8,4 +8,4 @@ const Layout = () => {
     );
 };
 
-export default Layout;
+export default UserContext;
