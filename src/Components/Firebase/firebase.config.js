5673d548b4d8a4f523ae7b494a0165fc8bccffbb
SOHELMAHMUD12 <sohelcse123@gmail.com>
2022-10-27 12:00:05 +0600

    added sign up form

---
+++ src/Components/Firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCvn9CB-q-egZjKpuFLG9ZrsgrXXB-GB8Y",
+  authDomain: "all-online-courses.firebaseapp.com",
+  projectId: "all-online-courses",
+  storageBucket: "all-online-courses.appspot.com",
+  messagingSenderId: "470200683914",
+  appId: "1:470200683914:web:b85f8a858b3123d1f8cc73"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
